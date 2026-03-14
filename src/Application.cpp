f6c7cb5869238dede38f094cd91c4a5b06aace55
LeviLe <li990835204@163.com>
2026-03-14 17:14:53 +0800

    opengl着色器uniform实验

--- src/Application.cpp
+++ src/Application.cpp
@@ -126,6 +126,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);    //VSync
+
     if (glewInit() != GLEW_OK)
     {
         std::cout << "ERROR" << std::endl;
@@ -165,18 +167,33 @@ int main(void)
     std::cout << source.FragmentSource << std::endl;
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != 1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 //        glDrawArrays(GL_TRIANGLES, 0, 6);
-
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f)); //uniform是整体的，不能让同一时间画出来的正方形一半是一种颜色另一半是另外一种
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
+        if(r > 1.0f)
+        {
+            increment = -0.05f;
+        }
+        else if(r <= 0.0f)
+        {
+            increment = 0.05f;
+        }
+        
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
